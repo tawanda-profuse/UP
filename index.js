94075a5129cd770136bbbbd4f677519b10bd8975
Tawanda Msengezi <xrthemc@gmail.com>
2021-05-27 06:22:24 +0200

    Changes to url paths

--- index.js
+++ index.js
@@ -4,20 +4,17 @@ $(document).ready(function(){
         var time = sunSet.getHours();
         var cloud = $('.cloud');
         // NIGHTIME
-        if(time >= 18){
+        if(time >= 18 && time <= 6){
             // holder.css("background-image", 'url(/images/test-1.jpeg)');
-            holder.css("background-image", 'url(/images/test-2.jpeg)');
+            holder.css("background-image", 'url(images/test-2.jpeg)');
             // holder.css("background-image", 'url(/images/test-3.jpeg)');
             // holder.css("background-image", 'url(/images/test-4.jpeg)');
             // holder.css("background-image", 'url(/images/test-5.png)');
             cloud.css("opacity", "0.1");
         }
-        else if(time <= 6){
-            holder.css("background-image", 'url(/images/test-2.jpeg)');
-        }
         // DAYTIME
         else{
-            holder.css("background-image", 'url(/images/image.jpeg)');
+            holder.css("background-image", 'url(images/image.jpeg)');
         }
     });
 
