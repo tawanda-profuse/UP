09594ad2aad9301526220ba48da549fa74e9d8b7
Tawanda Msengezi <xrthemc@gmail.com>
2021-06-09 08:31:23 +0200

    Adding more audio files, fixed nagging JS errror.

--- data.js
+++ data.js
@@ -4,5 +4,24 @@ var song_list = {
         duration: "1:22",
         track: "super mario bros theme song.mp3"
     },
-
+    "02": {
+        title: "Super Mario Sunshine",
+        duration: "1:39",
+        track: "super mario sunshine.mp3"
+    },
+    "03": {
+        title: "The Evil King Bowser",
+        duration: "1:40",
+        track: "the evil king bowser.mp3"
+    },
+    "04": {
+        title: "Mario Party 4 Soundtrack",
+        duration: "1:30",
+        track: "mario party 4 soundtrack.mp3"
+    },
+    "05": {
+        title: "Mario & Luigi Superstar Saga 🔥",
+        duration: "2:01",
+        track: "mario & luigi superstar saga.mp3"
+    },
 }
